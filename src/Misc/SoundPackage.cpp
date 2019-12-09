382b1a5cb70a144a9b6ae68729a5a16b2c66f9f7
rzeronte <eduardo.rzeronte@gmail.com>
2019-12-09 02:58:30 +0100

    REFACT - Minor refact / assets improvement

---
+++ src/Misc/SoundPackage.cpp
@@ -0,0 +1,45 @@
+
+#include "../../headers/Misc/SoundPackage.h"
+
+void SoundPackage::addItem(const std::string &srcSound, std::string label, SoundPackageItemType type)
+{
+    auto *item  = new SoundPackageItem();
+    item->type  = type;
+    item->label = std::move(label);
+
+    Mix_Chunk *sound;
+    Mix_Music *music;
+
+    switch (type) {
+        case SoundPackageItemType::SOUND:
+            sound = Mix_LoadWAV(srcSound.c_str() );
+            item->sound = sound;
+            break;
+        case SoundPackageItemType::MUSIC:
+            music = Mix_LoadMUS( srcSound.c_str() );
+            item->music = music;
+            break;
+        default:
+            assert(0);
+    }
+
+    this->items.push_back( item );
+}
+
+Mix_Chunk *SoundPackage::getSoundByLabel(const std::string &label)
+{
+    for (int i = 0; i < this->items.size(); i++ ) {
+        if (!items[i]->label.compare(label)) {
+            return items[i]->sound;
+        }
+    }
+}
+
+Mix_Music *SoundPackage::getMusicByLabel(const std::string &label)
+{
+    for (int i = 0; i < this->items.size(); i++ ) {
+        if (!items[i]->label.compare(label)) {
+            return items[i]->music;
+        }
+    }
+}
