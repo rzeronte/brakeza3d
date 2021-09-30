8902f8e429a67c6cc80b57b1d8bf293d2079a429
Eduardo Rodríguez <eduardo.rzeronte@gmail.com>
2021-09-30 18:58:54 +0200

    wip(windows): working on windows build

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
-#include <cstdio>
 #include "headers/Brakeza3D.h"
 
 #pragma pack(push, MAIN)
 
-#include <cstdlib>
 
 #pragma pack(pop, MAIN)
 
-int main(int argc, const char *args[]) {
+int main(int argc, char *argv[]) {
 
     Brakeza3D::get()->start();
     //Demo *demo = new Demo();
