59ed3db40bb75d90b0ecdc96a0e6ec3de88a13a5
Eduardo Rodríguez <e.rodriguez@docline.es>
2022-05-27 02:51:51 +0200

    CL Shaders

--- src/Components/ComponentRender.cpp
+++ src/Components/ComponentRender.cpp
@@ -3,11 +3,13 @@
 #include "../../include/Misc/Parallells.h"
 #include "../../include/Brakeza3D.h"
 
+
 void ComponentRender::onStart()
 {
     Logging::Log("ComponentRender onStart", "ComponentRender");
     initTiles();
     initializeShaders();
+    initOpenCL();
 }
 
 void ComponentRender::preUpdate()
@@ -23,6 +25,7 @@ void ComponentRender::preUpdate()
 
 void ComponentRender::onUpdate()
 {
+
     if (!isEnabled()) {
         return;
     }
@@ -65,7 +68,6 @@ void ComponentRender::onUpdate()
 }
 
 void ComponentRender::postUpdate() {
-
 }
 
 void ComponentRender::onEnd() {
@@ -1032,4 +1034,97 @@ void ComponentRender::onPostUpdateSceneObjects()
 
         object->postUpdate();
     }
+}
+
+void ComponentRender::initOpenCL()
+{
+    clPlatformId = NULL;
+    clDeviceId = NULL;
+
+    ret = clGetPlatformIDs(1, &clPlatformId, &ret_num_platforms) ;
+    if (ret != CL_SUCCESS) {
+        Logging::getInstance()->Log("Unable to get platform_id");
+    }
+
+    ret = clGetDeviceIDs(clPlatformId, CL_DEVICE_TYPE_GPU, 1, &clDeviceId, &ret_num_devices);
+    if (ret != CL_SUCCESS) {
+        Logging::getInstance()->Log("Unable to get cpu_device_id");
+    }
+
+    cl_context_properties properties[3];
+    properties[0]= CL_CONTEXT_PLATFORM;
+    properties[1]= (cl_context_properties) clPlatformId;
+    properties[2]= 0;
+
+    // Create an OpenCL context
+    clContext = clCreateContext(properties, 1, &clDeviceId, NULL, NULL, &ret);
+
+    clCommandQueue = clCreateCommandQueue(clContext, clDeviceId, NULL, &ret);
+
+    OpenCLInfo();
+}
+
+void ComponentRender::OpenCLInfo()
+{
+    int i, j;
+    char* value;
+    size_t valueSize;
+    cl_uint platformCount;
+    cl_platform_id* platforms;
+    cl_uint deviceCount;
+    cl_device_id* devices;
+    cl_uint maxComputeUnits;
+
+    // get all platforms
+    clGetPlatformIDs(0, NULL, &platformCount);
+    platforms = (cl_platform_id*) malloc(sizeof(cl_platform_id) * platformCount);
+    clGetPlatformIDs(platformCount, platforms, NULL);
+
+    for (i = 0; i < platformCount; i++) {
+
+        // get all devices
+        clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, NULL, &deviceCount);
+        devices = (cl_device_id*) malloc(sizeof(cl_device_id) * deviceCount);
+        clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, deviceCount, devices, NULL);
+
+        // for each device print critical attributes
+        for (j = 0; j < deviceCount; j++) {
+
+            // print device name
+            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 0, NULL, &valueSize);
+            value = (char*) malloc(valueSize);
+            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, valueSize, value, NULL);
+            printf("%d. Device: %s\n", j+1, value);
+            free(value);
+
+            // print hardware device version
+            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, 0, NULL, &valueSize);
+            value = (char*) malloc(valueSize);
+            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, valueSize, value, NULL);
+            printf(" %d.%d Hardware version: %s\n", j+1, 1, value);
+            free(value);
+
+            // print software driver version
+            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, 0, NULL, &valueSize);
+            value = (char*) malloc(valueSize);
+            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, valueSize, value, NULL);
+            printf(" %d.%d Software version: %s\n", j+1, 2, value);
+            free(value);
+
+            // print c version supported by compiler for device
+            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, 0, NULL, &valueSize);
+            value = (char*) malloc(valueSize);
+            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, valueSize, value, NULL);
+            printf(" %d.%d OpenCL C version: %s\n", j+1, 3, value);
+            free(value);
+
+            // print parallel compute units
+            clGetDeviceInfo(devices[j], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(maxComputeUnits), &maxComputeUnits, NULL);
+            printf(" %d.%d Parallel compute units: %d\n", j+1, 4, maxComputeUnits);
+        }
+
+        free(devices);
+    }
+
+    free(platforms);
 }
