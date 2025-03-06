cc8c1c3e3ea38c8bc414cb7a408d7a497d4aeac4
Eduardo <eduardo.rzeronte@gmail.com>
2025-03-06 21:19:26 +0100

    fix(depth): Added depth texture type to custom GLSL

--- GLSL/DepthMap.fs
+++ GLSL/DepthMap.fs
@@ -4,20 +4,19 @@ in vec2 TexCoords;
 out vec4 FragColor;
 
 uniform sampler2D depthTexture;
-
-uniform float far_plane;
 uniform float intensity;
-
-float near_plane = 0.1f;
+uniform float farPlane;
+uniform float nearPlane;
 
 float LinearizeDepth(float depth)
 {
-    float z = depth * 2.0 - 1.0;
-    return (2.0 * near_plane * far_plane) / (far_plane + near_plane - z * (far_plane - near_plane));
+    float z = depth * 2.0 - 1.0; // Convertir de [0,1] a [-1,1] (NDC)
+    return (2.0 * nearPlane * farPlane) / (farPlane + nearPlane - z * (farPlane - nearPlane));
 }
 
 void main()
 {
-    float depthValue = texture(depthTexture, vec2(TexCoords.x, 1.0 - TexCoords.y)).r * intensity;
-    FragColor = vec4(vec3(LinearizeDepth(depthValue) / far_plane), 1.0);
+    float depthSample = texture(depthTexture, vec2(TexCoords.x, 1.0 - TexCoords.y)).r;
+    float linearDepth = LinearizeDepth(depthSample);
+    FragColor = vec4(vec3((linearDepth / farPlane) * intensity), 1.0);
 }
