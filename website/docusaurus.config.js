618a6e40cadaaf50c7055727ebd9cf08e68d0a51
rzeronte <eduardo.rzeronte@gmail.com>
2026-01-18 18:59:34 +0100

    feat(website): improved doc

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://brakeza.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/brakeza3d',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
