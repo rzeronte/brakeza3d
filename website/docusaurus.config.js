4faa47459a90f4c79b01ac5d31cb56794b2c70dc
rzeronte <eduardo.rzeronte@gmail.com>
2026-01-18 18:54:08 +0100

    feat(website): improved doc

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://rzeronte.github.io',
+  url: 'https://brakeza.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/brakeza3d',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
